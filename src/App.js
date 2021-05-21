a2a0087fabccaaf120210dfdb7a88c3013ff7179
ESMAT001 <esmat10.niazi@gmail.com>
2021-05-22 00:27:25 +0430

    modal data and modal functionality added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import Login from "./components/login";
 import { Route } from "react-router-dom";
 import Index from "./components/Index";
-import AddNewMedicine from "./components/AddNewMedicine"
+import AddNewMedicine from "./components/AddNewMedicine";
 import "./tailwind.min.css";
 import "./style.css";
 import AddToPreviousMedicine from "./components/add_to_previous_medicine";
 import AddVisitor from "./components/add_visitor";
 import VisitorList from "./components/visitor_list";
 import AddNewInvoice from "./components/invoice/add_new_invoice";
-import AddNewCustomerInvoice from "./components/invoice/AddNewCustomerInvoice"
+import AddNewCustomerInvoice from "./components/invoice/AddNewCustomerInvoice";
+
 function App() {
   return (
     <>
-    <Route exact path="/" component={Index} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/addnewmedicine" component={AddNewMedicine} />
-    <Route exact path="/addtopreviousmedicine" component={AddToPreviousMedicine} />
-    <Route exact path="/addvisitor" component={AddVisitor} />
-    <Route exact path="/visitorlist" component={VisitorList} />
-    <Route exact path="/addnewinvoice" component={AddNewInvoice} />
-    <Route exact path="/AddNewCustomerInvoice" component={AddNewCustomerInvoice} />
-  </>
+      <Route exact path="/" component={Index} />{" "}
+      <Route exact path="/login" component={Login} />{" "}
+      <Route exact path="/addnewmedicine" component={AddNewMedicine} />{" "}
+      <Route
+        exact
+        path="/addtopreviousmedicine"
+        component={AddToPreviousMedicine}
+      />{" "}
+      <Route exact path="/addvisitor" component={AddVisitor} />{" "}
+      <Route exact path="/visitorlist" component={VisitorList} />{" "}
+      <Route exact path="/addnewinvoice" component={AddNewInvoice} />{" "}
+      <Route
+        exact
+        path="/AddNewCustomerInvoice"
+        component={AddNewCustomerInvoice}
+      />{" "}
+    </>
   );
 }
 
