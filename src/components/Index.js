e31a327bdaeb4500c65ac42383362ce3c078324c
ESMAT001 <esmat10.niazi@gmail.com>
2021-05-28 12:14:13 +0430

    data tables added to index page

--- src/components/Index.js
+++ src/components/Index.js
@@ -13,7 +13,8 @@ function Index() {
 
   const [rangeQ, setRangeQ] = useState(0);
   const [rangeE, setRangeE] = useState(0);
-
+  const [expireData, setExpireData] = useState([]);
+  const [quantityData, setQuantityData] = useState([]);
   useEffect(() => {
     axios
       .post(
@@ -26,6 +27,23 @@ function Index() {
         setRangeE(parseInt(res.data.data.medicine_range_expire));
         setRangeQ(parseInt(res.data.data.medicine_range_quantity));
       });
+
+    axios
+      .post(
+        "http://localhost:8080/pharmacyproject/backend/invoice_module/range.php",
+        {
+          method: "expire_date",
+        }
+      )
+      .then((res) => setExpireData(res.data.data));
+    axios
+      .post(
+        "http://localhost:8080/pharmacyproject/backend/invoice_module/range.php",
+        {
+          method: "quantity",
+        }
+      )
+      .then((res) => setQuantityData(res.data.data));
   }, []);
 
   async function saveData(e) {
@@ -63,17 +81,17 @@ function Index() {
                 } transition duration-500 p-4 rounded justify-between shadow-lg`}
               >
                 <label className="flex items-center space-x-4">
-                  <p className="font-semibold text-blue-500">Set Range</p>
+                  <p className="font-semibold text-gray-800">Set quantity Range</p>
                   <input
                     type="number"
                     value={rangeQ}
                     required={true}
                     onChange={(e) => setRangeQ(e.target.value)}
-                    className="border-2 border-blue-400 rounded py-1 px-2 focus:outline-none text-blue-500"
+                    className="border-2 border-blue-400 rounded py-1 px-2 focus:outline-none text-gray-800"
                   />
                 </label>
                 <label className="flex items-center space-x-4">
-                  <p className="font-semibold text-blue-500">
+                  <p className="font-semibold text-gray-800">
                     Set expire range (month)
                   </p>
                   <input
@@ -81,7 +99,7 @@ function Index() {
                     value={rangeE}
                     required={true}
                     onChange={(e) => setRangeE(e.target.value)}
-                    className="border-2 border-blue-400 rounded py-1 px-2 focus:outline-none text-blue-500"
+                    className="border-2 border-blue-400 rounded py-1 px-2 focus:outline-none text-gray-800"
                   />
                 </label>
                 <button
@@ -99,9 +117,61 @@ function Index() {
               <img src={settingIcon} alt="icon" className="w-10" />
             </button>
           </div>
-          <div className="flex flex-row justify-between mt-20">
-            <div>1</div>
-            <div>2</div>
+          <div className="flex flex-row justify-evenly mt-20">
+            <div className="bg-white py-4 px-7 rounded shadow-lg">
+              <h1 className="font-semibold text-lg text-gray-800">
+                List of medicine which will expire in {rangeE} months.
+              </h1>
+              <table className="w-full text-center mt-4">
+                <thead>
+                  <tr className="bg-blue-400 text-white shadow">
+                    <td className="py-1 px-2">#</td>
+                    <td>Product Name</td>
+                    <td>Expire Date</td>
+                  </tr>
+                </thead>
+                <tbody className="divide-y-2 divide-gray-200">
+                  {expireData.length &&
+                    expireData.map((el, i) => {
+                      return (
+                        <tr key={i}>
+                          <td className="py-1">{++i}</td>
+                          <td>{el.name}</td>
+                          <td>{el.expire_date}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
+            <div className="bg-white py-4 px-7 rounded shadow-lg">
+              <h1 className="font-semibold text-lg text-gray-800">
+                List of medicine which there quantity is less then {rangeQ}.
+              </h1>
+              <table className="w-full text-center mt-4">
+                <thead>
+                  <tr className="bg-blue-400 text-white shadow">
+                    <td className="py-1 px-2">#</td>
+                    <td>Product Name</td>
+                    <td>entry date</td>
+                    <td>Quantity</td>
+                  </tr>
+                </thead>
+                <tbody className="divide-y-2 divide-gray-200">
+                {quantityData.length &&
+                    quantityData.map((el, i) => {
+                      return (
+                        <tr key={i}>
+                          <td className="py-1">{++i}</td>
+                          <td>{el.name}</td>
+                          <td>{el.entry_date}</td>
+                          <td>{el.quantity}</td>
+                        </tr>
+                      );
+                    })}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>{" "}
       </div>{" "}
